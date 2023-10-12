9404eace644196f89116ac2ea789fbc2e940a772
MahdiTaremi <taremiam@gmail.com>
2023-10-12 16:31:35 +0330

    Add and Configure Swagger

--- Program.cs
+++ Program.cs
@@ -1,6 +1,21 @@
 var builder = WebApplication.CreateBuilder(args);
+
+// 1. Add Swagger 
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Add services to the container.
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// Configure the HTTP request pipeline.
+app.UseHttpsRedirection();
+
+//2.Add Swagger
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.Run();
