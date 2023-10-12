3818832861cb667f2dc32e38568be0e14206ad62
MahdiTaremi <taremiam@gmail.com>
2023-10-12 17:02:28 +0330

    Configure ConnectionString

--- Program.cs
+++ Program.cs
@@ -1,4 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+using TodoList.Infrastructure.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Connect to Sql Server (ConnectionStrings) :
+// 1 Way : ConnectionStrings
+// builder.Services.AddDbContext<DbContextTodoList>(x => x.UseSqlServer("Server=.;Database=TodoList;User Id=MahdiTaremi;Password=123;TrustServerCertificate=True;"));
+// 2 Way : ConnectionStrings
+builder.Services.AddDbContext<DbContextTodoList>(options =>
+{
+    var ConnectionStrings = builder.Configuration.GetConnectionString("Local");
+    options.UseSqlServer(ConnectionStrings);
+});
 
 // 1. Add Swagger 
 builder.Services.AddControllers();
---
+++ TodoList.Infrastructure/DataAccess/DbContextTodoList.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoList.Infrastructure.Data
+{
+    public class DbContextTodoList : DbContext
+    {
+        public DbContextTodoList(DbContextOptions<DbContextTodoList> options) : base(options)
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+        }
+    }
+}
