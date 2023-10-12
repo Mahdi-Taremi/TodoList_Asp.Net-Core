8d5f592d459e1ca69ece1a79d8d2fa74d9decff2
MahdiTaremi <taremiam@gmail.com>
2023-10-12 17:29:02 +0330

    Add Unit Test and Models

---
+++ TodoList.Core/Models/Todo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoList.Core.Models
+{
+    internal class Todo
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Please enter a Description")]
+        public string Description { get; set; }
+        [Required(ErrorMessage = "Please enter a Due Date")]
+        public DateTime? DueDate { get; set; }
+        [Required(ErrorMessage = "Please Select a Category")]
+        [ForeignKey(nameof(Category))]
+        public string CategoryId { get; set; }
+
+        public Category? Category { get; set; }
+
+        [Required(ErrorMessage = "Please Select a Status")]
+        [ForeignKey(nameof(Status))]
+        public string StatusId { get; set; }
+
+        public Status? Status { get; set; }
+        //public bool Overdue => StatusId == "ongoing" && DueDate < DateTime.Today;
+
+    }
+}
---
+++ TodoList.UnitTest/AddTaskUnitTest.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TodoList.UnitTest
+{
+    /// <summary>
+    /// Summary description for AddTaskUnitTest
+    /// </summary>
+    [TestClass]
+    public class AddTaskUnitTest
+    {
+        public AddTaskUnitTest()
+        {
+            //
+            // TODO: Add constructor logic here
+            //
+        }
+
+        [TestMethod]
+        public void TestMethodAddTask()
+        {
+            // Arrange
+            var task = new TodoList();
+            var item = new Todo();
+            task.AddTodo(item);
+
+            // Act 
+            task.Complete(item);
+
+            // Assert
+            task.Equal(0, task.Count);
+            //task.Equal(0, task.Items.Count());
+
+        }
+    }
+}
